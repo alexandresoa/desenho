0b6536b6a93750fabd9a8d092bdb0832abe9c332
Flávia Reis <flaviarster@gmail.com>
2024-03-09 12:00:11 -0300

    Fazendo o desenvolvimento da ferramenta

--- script.js
+++ script.js
@@ -2,4 +2,13 @@ const pencilButton = document.getElementById('pencil')
 const eraserButton = document.getElementById('eraser')
 const squareButton = document.getElementById('quadrado')
 const scissorsButton = document.getElementById('scissors')
-const buttons = document.querySelectorAll('button')
+const buttons = document.querySelectorAll('i')
+
+
+console.log(pencil, eraser, quadrado, scissors)
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        button.classList.toggle('active')
+    })
+})
